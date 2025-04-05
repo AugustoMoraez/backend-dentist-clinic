13916698fc5b5f2a326b07bcffe6ce361c83c11d
Augusto Moraes <supergamerbros2015@gmail.com>
2025-04-05 15:09:53 -0300

    table enterprise created

--- src/modules/empresa/entities/empresa.entity.ts
+++ src/modules/empresa/entities/empresa.entity.ts
@@ -1,1 +1,37 @@
-export class Empresa {}
+
+
+export class Address {
+    number:string;
+    street:string;
+    district:string;
+    state:string;
+    complement:string;
+}
+
+//Employee = Funcionario
+export class Employee{
+
+}
+//Patient = paciente
+export class Patient {
+
+}
+
+
+export class Empresa {
+    id:string;
+    email:string;
+    password:string
+    
+    razao:string;
+    fantasy_name:string;
+    cnpj:string;
+    cpf:string;
+    contact_1:string;
+    contact_2:string;
+    address:Address;
+
+    patient:Patient[];
+    employee:Employee[]
+
+}
