723f5709421f90b36d80d7b9c46ed8b31df3a59d
Augusto Moraes <supergamerbros2015@gmail.com>
2025-04-05 15:37:24 -0300

    ORM_MODULE created

---
+++ src/modules/sequelize/sequelize.module.ts
@@ -0,0 +1,11 @@
+import { SequelizeModule } from "@nestjs/sequelize";
+
+export const ORM_MODULE = SequelizeModule.forRoot({
+    dialect:"postgres",
+    username:"Dev",
+    password:"pq-0pEr7q2rLCKl90d6qIg",
+    host:"postgresql://Dev:pq-0pEr7q2rLCKl90d6qIg@battle-howler-5642.jxf.gcp-southamerica-east1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
+    port:26257,
+    database:"postgres",
+    synchronize: true
+  })
