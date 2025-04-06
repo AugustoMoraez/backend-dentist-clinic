8d79925d8b3b058dac84400e0649823b37b05f80
Augusto Moraes <supergamerbros2015@gmail.com>
2025-04-06 13:23:55 -0300

    prisma client configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmpresaModule } from './modules/enterprise/enterprise.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 
 @Module({
   imports: [
     EmpresaModule,
-   
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
