149321ef176caba0a046a3cad113f35b7a252a13
Augusto Moraes <supergamerbros2015@gmail.com>
2025-04-08 15:50:45 -0300

    new createEnterpriseDTO

--- src/modules/enterprise/dto/create-enterprise.dto.ts
+++ src/modules/enterprise/dto/create-enterprise.dto.ts
@@ -1,18 +1,13 @@
-export class CreateEnterpriseDto {
-    
-  email: string;
+import zod from "zod"
 
-  password: string;
-
-  company_name: string;
-
-  fantasy_name: string;
-
-  cnpj: string;
-
-  cpf: string;
-
-  contact_1: string;
-
-  contact_2: string;
-}
+export const createEnterpriseDTO = zod.object({
+  email:zod.string().email("Necessario um email") ,
+  password:zod.string().min(8,"minimo 8 caracteres"),
+  company_name: zod.string().max(45,"maximo de caracteres e 45").min(2).optional(),
+  fantasy_name:zod.string().max(45,"maximo de caracteres e 45").min(2).optional(),
+  cnpj:zod.string().min(14,"minimo de caracteres e 14").max(14,"maximo de caracteres e 14").optional() ,
+  cpf: zod.string().min(11,"minimo de caracteres e 11").max(11,"maximo de caracteres e 11").optional(),
+  contact_1:zod.string().min(15),
+  contact_2:zod.string().min(15).optional()
+  
+})
