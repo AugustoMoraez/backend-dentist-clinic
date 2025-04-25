4b6d3dfb5df88b6e91da6f27ac6040fc8298f6a6
Augusto Moraes <supergamerbros2015@gmail.com>
2025-04-25 17:14:46 -0300

    cors edit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
+import cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,10 @@ async function bootstrap() {
     transform:true
   }))
   app.use('/stripe/webhook', bodyParser.raw({ type: 'application/json' }));
+  app.use(cors({
+    origin: "http://localhost:5173",  
+    credentials: true
+  }))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
